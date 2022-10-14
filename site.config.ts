0da932a576d3cefbf554d381cb48f538fb269241
Rea1 <37218278+rea1zutter@users.noreply.github.com>
2022-10-14 12:06:39 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,14 +1,14 @@
 import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
-  // the site's root Notion page (required)
-  rootNotionPageId: '69300519aa2a465aa70f4f12edd1e3e0',
+  // the site's root Notion page (required) 下方更换为自己的notionID
+  rootNotionPageId: '6ae7a174a9694d7baef5c95cb109cec1',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: 'o-id',
 
-  // basic site info (required)
+  // basic site info (required) 编辑站点信息
   name: 'o-id',
   domain: 'o-id.cc',
   author: 'o-id',
@@ -16,7 +16,7 @@ export default siteConfig({
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
-  // social usernames (optional)
+  // social usernames (optional) 社交信息
   twitter: 'chusight1',
   github: 'chusight',
   zhihu: 'chusight',
